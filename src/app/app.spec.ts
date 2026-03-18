a306eb52fc8f4de1474031ebfca894806f65b296
kamilnaja <kamilnaja@gmail.com>
2026-03-18 21:39:40 +0100

    improving ci yml

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, draggable-cards');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Card 1');
   });
 });
